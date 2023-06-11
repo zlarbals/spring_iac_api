94dae2ca10b5468f5d44dc99a3f5a363f5d638e1
zlarbals <zlarbals@gmail.com>
2023-06-12 00:46:05 +0900

    create: 로그인 테스트 코드 추가

--- src/test/java/com/example/spring_iac_api/controller/MemberControllerTest.java
+++ src/test/java/com/example/spring_iac_api/controller/MemberControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.spring_iac_api.controller;
 
 import com.example.spring_iac_api.dto.MemberRequestDto;
+import com.example.spring_iac_api.service.MemberService;
+import com.example.spring_iac_api.util.PromisedReturnMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,18 +12,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class MemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private MemberService memberService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -66,4 +74,77 @@ class MemberControllerTest {
         resultActions.andExpect(status().isBadRequest());
     }
 
+    @DisplayName("로그인 테스트 - 정상 로그인")
+    @Test
+    public void testMemberSignIn() throws Exception {
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "1234@gmail.com";
+        String signInPassword = "12345678";
+        MemberRequestDto memberRequestDto = new MemberRequestDto(signInEmail,signInPassword);
+        String memberRequestDtoToJson = objectMapper.writeValueAsString(memberRequestDto);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/member/signin")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(memberRequestDtoToJson))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.email").value(signInEmail))
+                .andExpect(jsonPath("$.response.accessToken").exists())
+                .andExpect(jsonPath("$.response.refreshToken").exists());
+    }
+
+    @DisplayName("로그인 테스트 - 회원가입 안한 사용자")
+    @Test
+    public void testMemberSignInWithNotSignUpUser() throws Exception {
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "12345@gmail.com";
+        String signInPassword = "12345678";
+        MemberRequestDto memberRequestDto = new MemberRequestDto(signInEmail,signInPassword);
+        String memberRequestDtoToJson = objectMapper.writeValueAsString(memberRequestDto);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/member/signin")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(memberRequestDtoToJson))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorMessage").value(PromisedReturnMessage.FAIL_LOGIN));
+    }
+
+    @DisplayName("로그인 테스트 - 잘못된 패스워드")
+    @Test
+    public void testMemberSignInWithWrongPassword() throws Exception {
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "1234@gmail.com";
+        String signInPassword = "123456789";
+        MemberRequestDto memberRequestDto = new MemberRequestDto(signInEmail,signInPassword);
+        String memberRequestDtoToJson = objectMapper.writeValueAsString(memberRequestDto);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/member/signin")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(memberRequestDtoToJson))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorMessage").value(PromisedReturnMessage.FAIL_LOGIN));
+    }
 }
--- src/test/java/com/example/spring_iac_api/service/MemberServiceTest.java
+++ src/test/java/com/example/spring_iac_api/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.example.spring_iac_api.service;
 
 import com.example.spring_iac_api.domain.Member;
 import com.example.spring_iac_api.dto.MemberRequestDto;
+import com.example.spring_iac_api.dto.MemberResponseDto;
 import com.example.spring_iac_api.exception.MemberDuplicateException;
 import com.example.spring_iac_api.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
     @Autowired
@@ -71,6 +74,57 @@ class MemberServiceTest {
         assertNotEquals(password,member.getPassword());
         assertTrue(passwordEncoder.matches(password,member.getPassword()));
     }
+
+    @DisplayName("로그인 테스트 - 정상입력")
+    @Test
+    public void testMemberSignIn(){
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "1234@gmail.com";
+        String signInPassword = "12345678";
+
+        //when
+        MemberResponseDto memberResponseDto = memberService.signIn(new MemberRequestDto(signInEmail, signInPassword));
+
+        //then
+        assertEquals(memberResponseDto.getEmail(),signInEmail);
+    }
+
+    @DisplayName("로그인 테스트 - 회원가입 안한 사용자")
+    @Test
+    public void testMemberSignInWithNotSignUpUser(){
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "1234no@gmail.com";
+        String signInPassword = "12345678";
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class, () -> memberService.signIn(new MemberRequestDto(signInEmail, signInPassword)));
+    }
+
+    @DisplayName("로그인 테스트 - 잘못된 패스워드")
+    @Test
+    public void testMemberSignInWithWrongPassword(){
+        //case
+        String signUpEmail = "1234@gmail.com";
+        String signUpPassword = "12345678";
+        memberService.signUp(new MemberRequestDto(signUpEmail,signUpPassword));
+
+        String signInEmail = "1234@gmail.com";
+        String signInPassword = "123456789";
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class, () -> memberService.signIn(new MemberRequestDto(signInEmail, signInPassword)));
+    }
+
 
 
 
